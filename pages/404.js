97070c332621bb22f8a3096838dea749d1c46411
Elle Majors <hellolovely@ellemajors.com>
2023-07-09 13:14:19 -0600

    fix: layout shift

--- pages/404.js
+++ pages/404.js
@@ -1,12 +1,20 @@
 import Image from 'next/image'; 
 import gif from '../public/images/not-found.gif';
-import Layout from '../components/layout'
+import Layout from '../components/layout';
 
 export default function Custom404() {
   return(
     <Layout>
       <div className="container">
-        <Image src={gif} alt="404 - Page Not Found" />
+        <Image 
+          src={gif} 
+          alt="404 - Page Not Found"   
+          sizes="100vw"
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
         <h1>Oopsie!</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
         <p>Don't worry! I've got you. <a href="/">Home again</a>.</p>
@@ -15,9 +23,8 @@ export default function Custom404() {
             .container {
               display: flex;
               flex-direction: column;
-              align-items: center;
               justify-content: center;
-              height: 100vh;
+              align-items: center;
             }
             h1 {
               font-size: 3rem;
