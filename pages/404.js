a6645a9b5302a0778eee49a5f0441bb7db6de43c
Elle Majors <hellolovely@ellemajors.com>
2023-07-09 16:44:42 -0600

    add: back navigation link

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,14 @@
 import Image from 'next/image'; 
 import gif from '../public/images/not-found.gif';
 import Layout from '../components/layout';
+import { useRouter } from 'next/router';
 
 export default function Custom404() {
+  const router = useRouter();
+  const goBack = (e) => {
+    e.preventDefault();
+    router.back();
+  }
   return(
     <Layout>
       <div className="container">
@@ -17,7 +23,7 @@ export default function Custom404() {
         />
         <h1>Oopsie!</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
-        <p>Don't worry! I've got you. <a href="/">Home again</a>.</p>
+        <p>Don't worry! I've got you. <a href="/">Home</a> or <a href="#" onClick={goBack}>Back</a>?</p>
         <style jsx>
           {`
             .container {
